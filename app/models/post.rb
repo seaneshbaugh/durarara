60247e86fcd7a525e9dc2515b867441b65f85a6c
Sean Eshbaugh <seaneshbaugh@gmail.com>
2012-04-27 16:55:50 -0500

    Posts working without AJAX.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,36 @@
+require "digest/sha2"
+require "ridenticon"
+
 class Post < ActiveRecord::Base
   attr_accessible :body, :ip_address, :name, :subject
+
+  validates :body,
+    :presence => true
+
+  validates :ip_address,
+    :presence => true
+
+  validates :name,
+    :presence => true
+
+  validates :subject,
+    :presence => true
+
+  before_validation :set_name
+
+  def set_name
+    self.name = Digest::SHA2.hexdigest(self.ip_address)
+  end
+
+  def identicon
+    identicon_path = File.join(Rails.root, "public", "images", "identicon", "#{self.name}.png")
+
+    unless File.exist?(identicon_path)
+      image = RIdenticon::Identicon.new(self.name, :size => 100)
+
+      image.write(identicon_path)
+    end
+
+    File.join("/", "images", "identicon", "#{self.name}.png")
+  end
 end
