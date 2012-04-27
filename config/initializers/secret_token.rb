235211d54fdf6b0687aea990bdfebd55946400f4
Sean Eshbaugh <seaneshbaugh@gmail.com>
2012-04-27 01:42:13 -0500

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Durarara::Application.config.secret_token = '1ef296a08bafe6c8fcbb37feaa5849492dd7bf67deb8214cc64742f6fadcd822053e3a5e937aa033f0f06e35f145646c5a2a9e9eb7d8723e104c2f33e082f723'
