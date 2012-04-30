ed05de8dbbd6853c7f6da5d9e9f278127bbe3ca0
Sean Eshbaugh <seshbaugh@clickhere.com>
2012-04-30 17:10:06 -0500

    Cleaned up interface.

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -1,3 +1,29 @@
-$(function() {
+var refreshPostsTimeoutID;
 
+var refreshPosts = function() {
+	$.get("/", { latest_post_id: $("#latest_post_id").val() }, function(data, textStatus, jqXHR) {
+		// console.log(jqXHR);
+	}, "script");
+
+	refreshPostsTimeoutID = setTimeout(refreshPosts, 10000);
+};
+
+$(function() {
+	refreshPostsTimeoutID = setTimeout(refreshPosts, 10000);
+
+	$("#new_post").on("submit", function(event) {
+		event.preventDefault();
+
+		if ($("#post_subject").val() === "") {
+			$("#notice").text("You cannot post without a subject!").fadeIn(500).fadeOut(8000);
+		} else {
+			if ($("#post_body").val() === "") {
+				$("#notice").text("You cannot post without a body!").fadeIn(500).fadeOut(8000);
+			} else {
+				$.post($(this).attr("action"), $(this).serialize(), function(data, textStatus, jqXHR) {
+					// console.log(jqXHR);
+				}, "script");
+			}
+		}
+	});
 });
